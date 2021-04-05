2aeb74a33d97019899f7d91b74c84c477df729b2
Pravin B Shelar <pbshelar@fb.com>
2021-04-05 18:00:08 +0530

    fx

--- orc8r/gateway/python/setup.py
+++ orc8r/gateway/python/setup.py
@@ -93,7 +93,7 @@ setup(
         'jsonschema==3.1.0',
         "strict-rfc3339>=0.7",
         "rfc3987>=1.3.0",
-        "jsonpointer>=1.13",
+        "json-pointer>=1.13",
         "webcolors>=1.11.1",
     ],
     extras_require={
